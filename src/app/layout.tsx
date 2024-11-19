9ac637ae751de43bcd0468d34ae25301dd93ea10
Aman <amantrivedi598@gmail.com>
2024-11-19 02:17:01 -0800

    updated the readme.md

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ViewTransitions } from "next-view-transitions";
 import type { Metadata } from "next";
 import { UserRoleProvider } from "@/context/UserRoleContext";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Inter, Poppins } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
@@ -10,8 +10,6 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800"],
 });
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Adminsto Dashboard",

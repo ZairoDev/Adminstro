3a994de8842a17b8993ef07453ce606b17d73555
aviralm52 <aviralm52@gmail.com>
2025-08-01 14:42:09 +0530

    changes in getAllAgents route

--- src/app/api/addons/agents/getAllAgents/route.ts
+++ src/app/api/addons/agents/getAllAgents/route.ts
@@ -4,7 +4,7 @@ import Agents from "@/models/agent";
 
 export async function GET() {
   try {
-    const agents = await Agents.find({}, { agentName: 1, agentPhone: 1 });
+    const agents = await Agents.find();
 
     return NextResponse.json({ data: agents }, { status: 200 });
   } catch (err) {

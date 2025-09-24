9503896e8c179a17371033e05f06e9e0135daa9f
abhay6969 <abhaytripathi6969@gmail.com>
2025-09-24 19:19:49 +0530

    adding Not Replying page and review page Co-authored-by: Abhay <abhaytripathi6969@gmail.com>

--- src/util/type.ts
+++ src/util/type.ts
@@ -281,6 +281,7 @@ export interface IQuery {
   minBudget: number;
   maxBudget: number;
   leadQualityByCreator?: string;
+  leadQualityByTeamLead?: string;
   name: string;
   email: string;
   propertyShown?: number;
@@ -313,6 +314,7 @@ export interface IQuery {
   createdBy?: string;
   createdAt?: Date;
   updatedAt?: Date;
+  idName?: string;
 }
 
 export interface VisitInterface {
